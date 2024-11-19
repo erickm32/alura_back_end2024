eec16bbf7a7552827460b9c67be915d3b1fd0c1d
Érick Moreira <erick@sementesrastreadas.com.br>
2024-11-19 17:06:43 -0300

    Based on the db generated on mongodb cloud, updating the express server to connect to the db and return the movies as json on specific endpoints

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import express from 'express';
+import MongoConnection from './mongo_setup.js';
+import { ObjectId } from 'mongodb';
+
 
 const app = express();
+const connection = new MongoConnection();
 
 app.listen(3000, () => {
   console.log('server is on');
 });
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  console.log(req, res);
+app.get('/movies', async (req, res) => {
+  const movies = connection.db.collection('movies').find().limit(10);
   res.status(200);
-  res.send('potato');
+  const responseArray = await movies.toArray();
+  console.log(responseArray.length);
+  res.json(responseArray);
+})
+
+app.get('/movies/:id', async (req, res) => {
+  res.status(200);
+  const movieId = req.params.id;
+  const movie = await connection.db.collection('movies').findOne({ _id: new ObjectId(movieId) });
+  console.log(movie);
+  res.json(movie);
+});
+
+app.addListener('close', async () => {
+  console.log('closing mongo connection');
+  await connection.closeConnection();
+  console.log('closed mongo connection');
+  console.log('closing express connection');
 })
---
+++ mongo_setup.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export default class MongoConnection {
+  get db() { return this.client.db('sample_mflix'); }
+
+  constructor() {
+    const mongodbConnectionUri = this._setupCredentials();
+    console.log(mongodbConnectionUri);
+    this.client = new MongoClient(mongodbConnectionUri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+    this._run().then((_) => console.log('then')).catch(console.dir);
+  }
+
+  async closeConnection() {
+    // Ensures that the client will close when you finish/error
+    console.log('finnaly')
+    await this.client.close();
+  }
+
+  async _run() {
+      // Connect the client to the server	(optional starting in v4.7)
+      await this.client.connect();
+      // Send a ping to confirm a successful connection
+      await this.client.db("sample_mflix").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+
+  _setupCredentials() {
+    try {
+      return fs.readFileSync('mongo_access.txt', 'utf8');
+    } catch (err) {
+      console.error('Erro ao ler o arquivo:', err);
+    }
+  }
+}
+
